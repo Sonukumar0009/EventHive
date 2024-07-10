ac6cfa5f53978b86442fb665ae4684a4ee3b399f
Sonukumar0009 <sonukumarryder@gmail.com>
2024-07-10 06:08:02 +0530

    edited and commented

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -333,4 +333,15 @@
 // }
 
 // export default EventForm
-rafce
+import React from 'react'
+
+const EventForm = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default EventForm
+
